e22e2c29c8e5021c2529bf50211f244b1b80b255
Indhira <indhira.biogirl@gmail.com>
2019-05-06 15:37:23 -0500

    cache.matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,5 @@
-## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## makeCacheMatrix: This function creates a special "matrix" object that can 
+## cache its inverse.
 
 
 ## cacheSolve: This function computes the inverse of the special 
